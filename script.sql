8da573c4d9c7db7d19a2803a7c2661b715d54507
Gabriel Lavor <gvlavor@gmail.com>
2019-01-13 18:33:35 -0200

    script

--- script.sql
+++ script.sql
@@ -1,94 +1,142 @@
--- MySQL Workbench Forward Engineering
+-- MySQL dump 10.13  Distrib 5.7.24, for Linux (x86_64)
+--
+-- Host: localhost    Database: truckpad
+-- ------------------------------------------------------
+-- Server version	5.7.24-0ubuntu0.16.04.1
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema truckpad
--- -----------------------------------------------------
+--
+-- Table structure for table `checkin`
+--
 
--- -----------------------------------------------------
--- Schema truckpad
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `truckpad` DEFAULT CHARACTER SET utf8 ;
-USE `truckpad` ;
-
--- -----------------------------------------------------
--- Table `truckpad`.`tipo_veiculo`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `truckpad`.`tipo_veiculo` (
-  `codigo` INT NOT NULL,
-  `descricao` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`codigo`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `truckpad`.`motorista`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `truckpad`.`motorista` (
-  `codigo` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(255) NOT NULL,
-  `idade` INT NOT NULL,
-  `sexo` VARCHAR(1) NOT NULL,
-  `veiculo_proprio` TINYINT(1) NOT NULL,
-  `tipo_veiculo` INT NOT NULL,
-  `tipo_cnh` VARCHAR(2) NOT NULL,
+DROP TABLE IF EXISTS `checkin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `checkin` (
+  `codigo` int(11) NOT NULL AUTO_INCREMENT,
+  `data` timestamp NULL DEFAULT NULL,
+  `codigo_motorista` int(11) DEFAULT NULL,
   PRIMARY KEY (`codigo`),
-  INDEX `fk_motorista_1_idx` (`tipo_veiculo` ASC),
-  CONSTRAINT `fk_motorista_1`
-    FOREIGN KEY (`tipo_veiculo`)
-    REFERENCES `truckpad`.`tipo_veiculo` (`codigo`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  KEY `fk_checkin_1_idx` (`codigo_motorista`),
+  CONSTRAINT `fk_checkin_1` FOREIGN KEY (`codigo_motorista`) REFERENCES `motorista` (`codigo`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `checkin`
+--
 
--- -----------------------------------------------------
--- Table `truckpad`.`local`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `truckpad`.`local` (
-  `codigo` INT NOT NULL AUTO_INCREMENT,
-  `descricao` VARCHAR(45) NOT NULL,
-  `latitude` DECIMAL(11,8) NOT NULL,
-  `longitude` DECIMAL(11,8) NOT NULL,
-  PRIMARY KEY (`codigo`))
-ENGINE = InnoDB;
+LOCK TABLES `checkin` WRITE;
+/*!40000 ALTER TABLE `checkin` DISABLE KEYS */;
+INSERT INTO `checkin` VALUES (1,'2019-01-13 01:50:11',1),(2,'2019-01-12 12:00:00',2);
+/*!40000 ALTER TABLE `checkin` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `local`
+--
 
--- -----------------------------------------------------
--- Table `truckpad`.`checkin`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `truckpad`.`checkin` (
-  `codigo` INT NOT NULL AUTO_INCREMENT,
-  `data` TIMESTAMP NULL,
-  `codigo_origem` INT NULL,
-  `codigo_destino` INT NULL,
-  `carregado` TINYINT(1) NULL,
-  `codigo_motorista` INT NULL,
+DROP TABLE IF EXISTS `local`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `local` (
+  `codigo` int(11) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(45) NOT NULL,
+  `latitude` decimal(11,8) NOT NULL,
+  `longitude` decimal(11,8) NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `local`
+--
+
+LOCK TABLES `local` WRITE;
+/*!40000 ALTER TABLE `local` DISABLE KEYS */;
+INSERT INTO `local` VALUES (1,'EXTRA',-57.55500000,-55.98770000);
+/*!40000 ALTER TABLE `local` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `motorista`
+--
+
+DROP TABLE IF EXISTS `motorista`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `motorista` (
+  `codigo` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `idade` int(11) NOT NULL,
+  `sexo` varchar(1) NOT NULL,
+  `veiculo_proprio` tinyint(1) NOT NULL,
+  `tipo_veiculo` int(11) NOT NULL,
+  `tipo_cnh` varchar(2) NOT NULL,
+  `carregado` int(11) NOT NULL,
+  `codigo_origem` int(11) NOT NULL,
+  `codigo_destino` int(11) NOT NULL,
   PRIMARY KEY (`codigo`),
-  INDEX `fk_checkin_1_idx` (`codigo_motorista` ASC),
-  INDEX `fk_checkin_2_idx` (`codigo_origem` ASC),
-  INDEX `fk_checkin_3_idx` (`codigo_destino` ASC),
-  CONSTRAINT `fk_checkin_1`
-    FOREIGN KEY (`codigo_motorista`)
-    REFERENCES `truckpad`.`motorista` (`codigo`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_checkin_2`
-    FOREIGN KEY (`codigo_origem`)
-    REFERENCES `truckpad`.`local` (`codigo`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_checkin_3`
-    FOREIGN KEY (`codigo_destino`)
-    REFERENCES `truckpad`.`local` (`codigo`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  KEY `fk_motorista_1_idx` (`tipo_veiculo`),
+  KEY `fk_motorista_3_idx` (`codigo_destino`),
+  KEY `fk_motorista_2_idx` (`codigo_origem`),
+  CONSTRAINT `fk_motorista_1` FOREIGN KEY (`tipo_veiculo`) REFERENCES `tipo_veiculo` (`codigo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_motorista_2` FOREIGN KEY (`codigo_origem`) REFERENCES `local` (`codigo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_motorista_3` FOREIGN KEY (`codigo_destino`) REFERENCES `local` (`codigo`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `motorista`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+LOCK TABLES `motorista` WRITE;
+/*!40000 ALTER TABLE `motorista` DISABLE KEYS */;
+INSERT INTO `motorista` VALUES (1,'Gabriel',21,'M',1,1,'A',1,1,1),(2,'Jose',50,'M',1,1,'B',0,1,1);
+/*!40000 ALTER TABLE `motorista` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tipo_veiculo`
+--
+
+DROP TABLE IF EXISTS `tipo_veiculo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tipo_veiculo` (
+  `codigo` int(11) NOT NULL,
+  `descricao` varchar(45) NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tipo_veiculo`
+--
+
+LOCK TABLES `tipo_veiculo` WRITE;
+/*!40000 ALTER TABLE `tipo_veiculo` DISABLE KEYS */;
+INSERT INTO `tipo_veiculo` VALUES (1,'Caminhão 3/4'),(2,'Caminhão Toco'),(3,'Caminhão Truck'),(4,'Carreta Simples'),(5,'Carreta Eixo Extendido');
+/*!40000 ALTER TABLE `tipo_veiculo` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-01-13 18:30:06
